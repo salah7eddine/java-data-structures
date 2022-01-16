2180aca9ae5b01c49a220b7e81094f8af575e5fd
ABOUSAID Salah-Eddine <salah-eddine.abousaid@capgemini.com>
2022-01-16 11:21:27 +0100

    Working With LinkedList

--- src/main/java/org/home/WorkingWithQueues.java
+++ src/main/java/org/home/WorkingWithQueues.java
@@ -1,11 +1,36 @@
 package org.home;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Queue;
 
 public class WorkingWithQueues {
     public static void main(String[] args) {
+        // queues();
 
+        LinkedList<Person> linkedList = new LinkedList<>();
+
+        linkedList.add(new Person("Alex", 21));
+        linkedList.add(new Person("Alexa", 21));
+        linkedList.addFirst(new Person("Ali", 18));
+
+        ListIterator<Person> personListIterator = linkedList.listIterator();
+
+        System.out.println("Next");
+
+        while (personListIterator.hasNext()) {
+            System.out.println(personListIterator.next());
+        }
+
+        System.out.println("Previous");
+
+        while (personListIterator.hasPrevious()) {
+            System.out.println(personListIterator.previous());
+        }
+
+    }
+
+    private static void queues() {
         Queue<Person> queue = new LinkedList<>();
         queue.add(new Person("Alex", 21));
         queue.add(new Person("Mariam", 24));
@@ -22,4 +47,6 @@ public class WorkingWithQueues {
     static record Person(String name, int age) {
 
     }
+
+
 }
